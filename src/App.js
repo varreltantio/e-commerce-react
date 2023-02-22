86682e0d54e8c103199cae660bbb514c3445537c
varreltantio <snetid90@gmail.com>
2023-02-22 14:47:49 +0700

    update .gitignore

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { commerce } from "./lib/commerce";
 import { Products, Navbar, Cart, Checkout } from "./components";
 
